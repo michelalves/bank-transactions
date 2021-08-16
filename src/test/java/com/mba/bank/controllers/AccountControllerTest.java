4a0f7fcb03f844c0c39734844fcdc9b0ed15f86b
Michel <michel.alves@contaazul.com>
2021-08-16 14:15:02 -0300

    fix: conflict on test configuration between @EnableJpaAuditing and @WebMvcTest

--- src/test/java/com/mba/bank/controllers/AccountControllerTest.java
+++ src/test/java/com/mba/bank/controllers/AccountControllerTest.java
@@ -6,21 +6,25 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mba.bank.dtos.AccountDTO;
+import com.mba.bank.services.AccountService;
 
 @ExtendWith(SpringExtension.class)
-@WebMvcTest(AccountController.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class AccountControllerTest {
 
   private static final String EXPECTED_CONTENT =
       "{\"account_id\":1,\"document_number\":\"04725128970\"}";
-  // @MockBean private AccountService accountService;
+  @MockBean private AccountService accountService;
   @Autowired private MockMvc mockMvc;
   @Autowired private ObjectMapper objectMapper;
 
